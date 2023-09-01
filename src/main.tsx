cff16d318fe9c3f03fb8cfd2436fd5f14332cb7a
MartinBINARD <martin.binard.dev@gmail.com>
2023-09-01 10:41:04 +0200

    6. Store : préparation du Reducer && 7. Provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
-import App from './components/App/App';
-
 import store from './store';
+
+import App from './components/App/App';
 
 import './styles/index.scss';
 
---
+++ src/store/reducers/chat.ts
@@ -0,0 +1,31 @@
+import { createReducer } from '@reduxjs/toolkit';
+
+import { Message } from '../../@types';
+
+interface ChatState {
+  messages: Message[];
+}
+
+const initialState: ChatState = {
+  messages: [
+    {
+      id: 1,
+      author: 'Super Chat',
+      content: 'Salut ça va ?',
+    },
+    {
+      id: 2,
+      author: 'Super Chat',
+      content: "T'as pas des super-croquettes ?",
+    },
+    {
+      id: 3,
+      author: 'Super Chat',
+      content: 'Stp',
+    },
+  ],
+};
+
+const chatReducer = createReducer(initialState, (builder) => {});
+
+export default chatReducer;
